3faf36a0b205740e8e5ce6e33b6db9c2e7c91d37
長谷川良 <hase.industries@i.softbank.jp>
2018-07-19 00:28:03 +0900

    課題提出用

---
+++ SecondKadaiApp/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  SecondKadaiApp
+//
+//  Created by HaseMac on 2018/07/18.
+//  Copyright © 2018年 ryou.hasegawa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    //値を受け取るプロパティ
+    var name:String = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("viewDidLoad: \(name)")
+        label.text = "こんにちは、\(name)さん"
+        }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+
+    }
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //遷移先SecondViewControllerを取得
+        let secondViewController:SecondViewController = segue.destination as! SecondViewController
+        //遷移先のSecondViewControllerで宣言しているnameに値を代入して渡す
+        secondViewController.name = textField.text!
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        // 他の画面から segue を使って戻ってきた時に呼ばれる
+    }
 
 }
 
